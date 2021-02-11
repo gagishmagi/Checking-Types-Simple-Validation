028d029a035749af5e1dc905ea315c69eec69e6b
gagi <gagi.shmagi@gmail.com>
2021-02-11 09:00:22 +0200

    Project checking PropTypes done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import  React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render(){
+    return (
+      <div className="App">
+        <h1>ReactJS props validation example</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Type</th>
+              <th>Value</th>
+              <th>Valid</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Array</td>
+              <td>{this.props.propArray}</td>
+              <td>{this.props.propArray ? "true" : "False"}</td>
+            </tr>
+            <tr>
+              <td>Boolean</td>
+              <td>{this.props.propBool ? "true" : "False"}</td>
+              <td>{this.props.propBool ? "true" : "False"}</td>
+            </tr>
+            <tr>
+              <td>Number</td>
+              <td>{this.props.propNum}</td>
+              <td>{this.props.propNum ? "true" : "False"}</td>
+            </tr>
+            <tr>
+              <td>Function</td>
+              <td>{this.props.propFunc("Yuri")}</td>
+              <td>{this.props.propFunc("Yuri") ? "true" : "False"}</td>
+            </tr>
+            <tr>
+              <td>String</td>
+              <td>{this.props.propString}</td>
+              <td>{this.props.propString ? "true" : "False"}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+
 }
+
+App.propTypes = {
+  propArray: PropTypes.array.isRequired,
+  propBool: PropTypes.bool.isRequired,
+  propNum: PropTypes.number,
+  propFunc: PropTypes.func,
+  propString: PropTypes.string
+}
+
+App.defaultProps = {
+  propArray: [],
+  propBool: false,
+  propNum: "-1",
+  propFunc: function(name){ return "Hello"+name},
+  propString: "asdasdasdasdas"
+}
+
+
 
 export default App;
